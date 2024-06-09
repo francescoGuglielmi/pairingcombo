1b21e594a38ea304be9b39fc813b19c5136fcd72
Francesco Guglielmi <francescoguglielmi@MacBook-Pro.local>
2024-06-09 21:25:15 +0100

    initialise backend spring boot boilerplate code

---
+++ backend/pairingcombo/src/main/java/com/pairingcombo/pairingcombo/PairingcomboApplication.java
@@ -0,0 +1,13 @@
+package com.pairingcombo.pairingcombo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PairingcomboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PairingcomboApplication.class, args);
+	}
+
+}
